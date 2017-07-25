bff06aba0418b01f4ac3946aa0b07d60ae032cb7
austinChappell <Austin.W.Chappell@gmail.com>
2017-07-25 13:58:14 -0500

    add two variables

--- app.js
+++ app.js
@@ -8,12 +8,17 @@ app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res) {
-  let person = {
+  let person1 = {
     name: 'Austin',
     age: 32,
-    hair: 'bald'
+    hair: 'brown'
   }
-  res.render('main', person);
+  let person2 = {
+    name: 'Lauren',
+    age: 30,
+    hair: 'brown'
+  }
+  res.render('main', {person1, person2});
 });
 
 app.listen(3000, function() {
